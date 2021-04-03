f6953d511c3cb92a0180a79a76a6fd481a8c29a8
unknown <alineeuu@outlook.com>
2021-04-03 08:22:57 -0300

    nova tentativa, ainda errado

--- Python/1340.py
+++ Python/1340.py
@@ -1,33 +1,40 @@
-eof_fake = True
-while eof_fake:
-    lista,lista_auxiliar,pontos = [],[],0
-    vezes = int(input())
 
-    for i in range(vezes):
-        instruc,element = input().split()
-        if instruc == '1':
-            lista.append(int(element))
-        else:
-            lista_auxiliar.append(int(element))
+while True:
+    try:
+        lista,lista_auxiliar,pontos = [],[],0
+        vezes = int(input())
 
-    if lista == lista_auxiliar:
-        nome = 'queue'
-        pontos += 1
-   
-    if lista == lista_auxiliar[::-1]:
-        nome = 'stack'
-        pontos += 1
+        for i in range(vezes):
+            instruc,element = input().split()
+            if instruc == '1':
+                lista.append(int(element))
+            else:
+                lista_auxiliar.append(int(element))
 
-    lista.sort()
-
-    if lista[::-1] == lista_auxiliar:
-        nome = 'priority queue'
-        pontos += 1
-
-    if pontos > 1:
-        nome = 'not sure'
-
-    if pontos == 0:
-        nome = 'impossible'
+        if lista == lista_auxiliar:
+            nome = 'queue'
+            pontos += 1
     
-    print(nome)
+        if lista == lista_auxiliar[::-1]:
+            nome = 'stack'
+            pontos += 1
+
+        lista.sort(reverse=True)
+        auxiliar = True
+        for i in range(len(lista_auxiliar)):
+            if lista_auxiliar[i] != lista[i]:
+                auxiliar = False
+
+        if auxiliar:
+            nome = 'priority queue'
+            pontos += 1
+
+        if pontos > 1:
+            nome = 'not sure'
+
+        if pontos == 0:
+            nome = 'impossible'
+        
+        print(nome)
+    except EOFError:
+        break
